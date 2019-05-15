8d867f1461ace821e1774a93523abd7c9fdd4939
Aikiro42 <38589158+Aikiro42@users.noreply.github.com>
2019-05-15 13:42:44 +0800

    added root folder

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<title>My SCALE Portfolio</title>
+		<link rel="stylesheet" type="text/css" href="css/var.css" />
+		<link rel="stylesheet" type="text/css" href="css/main.css" />
+		<link rel="stylesheet" type="text/css" href="css/introduction.css" />
+		<link rel="stylesheet" type="text/css" href="css/activity.css" />
+		<script src="jquery-3.3.1.min.js"></script>
+		<script src="jquery.rotate.js"></script>
+		<script src="js/main.js"></script>
+	</head>
+	<body>
+		<div id="media-viewer">
+			<img id="close-viewer" src="img/x-icon.png" />
+			<div id="media-viewer-container">
+			</div>
+		</div>
+		<header>
+			<img class="emoji clap" src="img/emoji-clap.png"/>
+			<img class="emoji clap" src="img/emoji-clap.png"/>
+			<span class="scale-letter">SCALE</span>
+			<span id="review">review</span>
+			<span class="hint"></span>
+		</header>
+		<div id="wrapper" style="display: none;">
+		</div>
+		<div id="activity-wrapper" style="display:none;">
+		</div>
+	
+	</body>
+</html>
